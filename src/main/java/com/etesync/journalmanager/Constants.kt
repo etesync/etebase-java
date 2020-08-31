3539cd904968e05f9b7ca9a2f55b587775310213
Tom Hacohen <tom@stosb.com>
2020-08-31 12:41:02 +0300

    Remove legacy implementation (pushing to branch legacy)

--- src/main/java/com/etesync/journalmanager/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.etesync.journalmanager
-
-class Constants {
-    companion object {
-        @JvmField
-        val CURRENT_VERSION = 2
-    }
-}
--- src/main/java/com/etesync/journalmanager/Logger.kt
+++
@@ -1,5 +0,0 @@
-package com.etesync.journalmanager
-
-object Logger {
-    val log = java.util.logging.Logger.getLogger("etesync")!!
-}
