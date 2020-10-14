ca6eca488c7aaf520f7aa5dbd444d3185a53711b
Tom Hacohen <tom@stosb.com>
2020-10-14 14:49:02 +0300

    Update etebase dep and adjust code accordingly.

--- client/src/androidTest/java/com/etebase/client/Service.java
+++ client/src/androidTest/java/com/etebase/client/Service.java
@@ -19,11 +19,12 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @RunWith(AndroidJUnit4.class)
 public class Service {
     private static String storedSession = "gqd2ZXJzaW9uAa1lbmNyeXB0ZWREYXRhxQGr_KWyDChQ6tXOJwJKf0Kw3QyR99itPIF3vZ5w6pVXSIq7AWul3fIXjIZOsBEwTVRumw7e9Af38D5oIL2VLNPLlmTOMjzIvuB00z3zDMFbH8pwrg2p_FvAhLHGjUGoXzU2XIxS4If7rQUfEz1zWkHPqWMrj4hACML5fks302dOUw7OsSMekcQaaVqMyj82MY3lG2qj8CL6ykSED7nW6OYWwMBJ1rSDGXhQRd5JuCGl6kgAHxKS6gkkIAWeUKjC6-Th2etk1XPKDiks0SZrQpmuXG8h_TBdd4igjRUqnIk09z5wvJFViXIU4M3pQomyFPk3Slh7KHvWhzxG0zbC2kUngQZ5h-LbVTLuT_TQWjYmHiOIihenrzl7z9MLebUq6vuwusZMRJ1Atau0Y2HcOzulYt4tLRP49d56qFEId3R4xomZ666hy-EFodsbzpxEKHeBUro3_gifOOKR8zkyLKTRz1UipZfKvnWk_RHFgZlSClRsXyaP34wstUavSiz-HNmTEmflNQKM7Awfel108FcSbW9NQAogW2Y2copP-P-R-DiHThrXmgDsWkTQFA";
-    private static String SERVER_URL = "http://10.100.102.6:8033";
+    private static String SERVER_URL = "http://10.100.102.8:8033";
     private static OkHttpClient httpClient =  new OkHttpClient.Builder()
             // don't allow redirects by default, because it would break PROPFIND handling
             .followRedirects(false)
             .build();
+    private static String COL_TYPE = "some.coltype";
 
     @Test
     public void testSmoketest() {
@@ -32,17 +33,17 @@ public class Service {
         etebase.forceServerUrl(SERVER_URL);
         etebase.fetchToken();
         CollectionManager col_mgr = etebase.getCollectionManager();
-        CollectionMetadata collectionMetadata = new CollectionMetadata("Type", "Name");
-        Collection col = col_mgr.create(collectionMetadata, "Something".getBytes());
+        ItemMetadata collectionMetadata = new ItemMetadata();
+        Collection col = col_mgr.create(COL_TYPE, collectionMetadata, "Something".getBytes());
         byte[] content = col.getContent();
         String str = new String(content, UTF_8);
         assertEquals(str, "Something");
         FetchOptions fetchOptions = new FetchOptions().prefetch(PrefetchOption.Auto);
         col_mgr.upload(col, fetchOptions);
-        CollectionListResponse col_list = col_mgr.list(null);
+        CollectionListResponse col_list = col_mgr.list(COL_TYPE, null);
         assertNotEquals(col_list.getData().length, 0);
         fetchOptions = new FetchOptions().stoken(col_list.getStoken());
-        col_list = col_mgr.list(fetchOptions);
+        col_list = col_mgr.list(COL_TYPE, fetchOptions);
         assertEquals(col_list.getData().length, 0);
 
         Collection col2 = col_mgr.fetch(col.getUid(), null);
@@ -84,12 +85,12 @@ public class Service {
         etebase.forceServerUrl(SERVER_URL);
         etebase.fetchToken();
         CollectionManager col_mgr = etebase.getCollectionManager();
-        CollectionMetadata collectionMetadata = new CollectionMetadata("Type", "Name");
-        Collection col = col_mgr.create(collectionMetadata, "Something".getBytes());
+        ItemMetadata collectionMetadata = new ItemMetadata();
+        Collection col = col_mgr.create(COL_TYPE, collectionMetadata, "Something".getBytes());
         col_mgr.upload(col, null);
-        CollectionListResponse col_list = col_mgr.list(null);
+        CollectionListResponse col_list = col_mgr.list(COL_TYPE);
         FetchOptions fetchOptions = new FetchOptions().stoken(col_list.getStoken());
-        col_list = col_mgr.list(fetchOptions);
+        col_list = col_mgr.list(COL_TYPE, fetchOptions);
         assertEquals(0, col_list.getRemovedMemberships().length);
 
         // FIXME: actually test we get the removed memberships ever.
@@ -104,8 +105,8 @@ public class Service {
         etebase.forceServerUrl(SERVER_URL);
         etebase.fetchToken();
         CollectionManager col_mgr = etebase.getCollectionManager();
-        CollectionMetadata collectionMetadata = new CollectionMetadata("Type","Name");
-        Collection col = col_mgr.create(collectionMetadata, "Something".getBytes());
+        ItemMetadata collectionMetadata = new ItemMetadata();
+        Collection col = col_mgr.create(COL_TYPE, collectionMetadata, "Something".getBytes());
         col_mgr.upload(col,null);
 
         String cached = etebase.save(null);
@@ -115,7 +116,7 @@ public class Service {
         assertArrayEquals(col.getContent(), "Something".getBytes());
         byte[] cachedCol = col_mgr.cacheSave(col);
         col = col_mgr.cacheLoad(cachedCol);
-        assertEquals(col.getMeta().getCollectionType(), "Type");
+        assertEquals(col.getCollectionType(), COL_TYPE);
 
         ItemManager it_mgr = col_mgr.getItemManager(col);
         ItemMetadata itemMetadata = new ItemMetadata();
@@ -142,8 +143,8 @@ public class Service {
         Account etebase = Account.restore(client, storedSession, null);
 
         CollectionManager colMgr = etebase.getCollectionManager();
-        CollectionMetadata collectionMetadata = new CollectionMetadata("Type", "Name");
-        Collection col = colMgr.create(collectionMetadata, "Something");
+        ItemMetadata collectionMetadata = new ItemMetadata();
+        Collection col = colMgr.create(COL_TYPE, collectionMetadata, "Something");
         assertEquals(col.getContentString(), "Something");
         col.setContent("Another");
         assertEquals(col.getContentString(), "Another");
